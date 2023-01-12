b8fdd5b8fcb6d8887bdf2d8a960be7349a0f117a
Dimaua <dimarugby@gmail.com>
2023-01-12 22:30:06 +0200

    something has changed

---
+++ index.js
@@ -0,0 +1,20 @@
+let str = 'Rolling Scopes School'
+
+/* console.log(str.length)
+let i = 0, c = 0;
+while ( i< str.length) {
+    if (str[i] === 'o') 
+    { c++; } 
+    i++;
+}
+console.log(c); */
+
+console.log('Vasia'.toUpperCase().length.toString())
+console.log('A' > 'a')
+const name = 'Vasia';
+console.log(Math.abs(Math.floor(-46.867)))
+console.log(Boolean(''))
+const RS = 'rs'; console.log('css', RS, 'js')
+
+let a,b,c;
+console.log(NaN + 101)
